18587d5199c635900eecaaf294c66d2ed891c9b5
ricardopimentel <ricardoppimentel@gmail.com>
2025-07-14 15:14:51 -0300

    Update ChatList.vue
    fgh

--- app/javascript/dashboard/components/ChatList.vue
+++ app/javascript/dashboard/components/ChatList.vue
@@ -269,9 +269,9 @@ const conversationListPagination = computed(() => {
 });
 
 const conversationFilters = computed(() => {
-  // Primeiro, criamos um objeto base com os filtros comuns
-  const filters = {
+  return {
     inboxId: props.conversationInbox ? props.conversationInbox : undefined,
+    assigneeType: activeAssigneeTab.value,
     status: activeStatus.value,
     sortBy: activeSortBy.value,
     page: conversationListPagination.value,
@@ -279,22 +279,6 @@ const conversationFilters = computed(() => {
     teamId: props.teamId || undefined,
     conversationType: props.conversationType || undefined,
   };
-
-  // AGORA, A MUDANÇA CRÍTICA:
-  // Nós só adicionamos o filtro 'assigneeType' se a aba ativa NÃO for a 'Minhas'.
-  if (activeAssigneeTab.value !== 'me') {
-    filters.assigneeType = activeAssigneeTab.value;
-  }
-
-  // Retornamos o objeto de filtros modificado.
-  return filters;
-});
-
-const activeTeam = computed(() => {
-  if (props.teamId) {
-    return getTeamFn.value(props.teamId);
-  }
-  return {};
 });
 
 const pageTitle = computed(() => {
